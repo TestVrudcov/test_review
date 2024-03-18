f79448dd156cf13ff6f42895e7082b2fe77ed9e0
Vika Rudkovskaya <vrudcov@gmail.com>
2024-03-18 13:02:20 +0300

    Comment 13
    13.  К строке 73: если в "join" есть "and", то он переносится на следующую строку с отступом

--- Задание для кандидатов. Выполнить ревью кода.sql
+++ Задание для кандидатов. Выполнить ревью кода.sql
@@ -69,6 +69,7 @@ begin
 	-- 12. Блок с "left join" ниже должен идти с отступом
 	left join dbo.Customer as c on c.UID_DS = cs.UID_DS_Customer
 		and c.ID_mapping_DataSource = 1
+	-- 13. Если в "join" есть "and", то он переносится на следующую строку с отступом
 	left join dbo.Customer as c_dist on c_dist.UID_DS = cs.UID_DS_CustomerDistributor and c_dist.ID_mapping_DataSource = 1
 	left join dbo.Season as s on s.Name = cs.Season
 	left join syn.CustomerSystemType as cst on cst.Name = cs.CustomerSystemType
